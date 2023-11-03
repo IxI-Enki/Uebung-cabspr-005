2dab7e1d99fc7ccff9d445a844f062d4453ada9a
Enki <138018029+IxI-Enki@users.noreply.github.com>
2023-11-03 17:44:22 +0100

    Add files via upload

--- Temperaturtabelle.c
+++ Temperaturtabelle.c
@@ -3,67 +3,29 @@
  *------------------------------------------------------------------------------
  *                      Jan Ritt                                                
  *------------------------------------------------------------------------------
- *  Description:  A simple calculator.
+ *  Description:  The program outputs a temperature scale from +50°C to -50°C   
+ *                It displays Celsius - Fahrenheit                              
+ *                In 10°C steps
  *----------------------------------------------------------------------------*/
 #include <stdio.h>     // input/output library    -  printf, scanf, ..
 #include <string.h>    // string handling library -  strlen, strcpy, strcmp, ..
 int main(void)
 { 
-  /*  VARIABLES  */
-  int numberOne;       // first Operand
-  int numberTwo;       // second Operand
-  int resultModulo;    // calculation result if modulo is calculated
-  float result;        // calculation result
-  char operator[2];    // Operator
-  
-  //  INPUT  //
-  printf("\n Einfacher Taschenrechner \n");
-  printf("==========================\n");
-  printf("Geben Sie den ersten ganzzahligen Operanden ein: ");
-  scanf("%i",&numberOne);
-  printf("\nGeben Sie den zweiten ganzzahligen Operanden ein: ");
-  scanf("%i",&numberTwo);
-  printf("\nWaehlen Sie einen Operator (+ - * / %%): ");
-  scanf("%1s",operator);
+  // VARIABLES //
+  int i;
+
   /*  CALCULATION  */
-  if (strlen (operator) == 1        //  test if the operator is 1 char long
-          && (operator[0] == '+'    //  & test for allowed symbols (+ - * / %)
-          || operator[0] == '-'     //
-          || operator[0] == '*'     //
-          || operator[0] == '/'     //
-          || operator[0] == '%'))   //
-  {                                                                                                                        
-    if (operator[0] == '+')
-    {
-      result = numberOne + numberTwo;
-    }
-    else if (operator[0] == '-')
-    {
-      result = numberOne - numberTwo;
-    }
-    else if (operator[0] == '*')
-    {
-      result = numberOne * numberTwo;      
-    }
-    else if (operator[0] == '/')
-    {
-      result = numberOne / numberTwo;      
-    }
-        else if (operator[0] == '%')
-    {
-       resultModulo = numberOne % numberTwo;
-       result = resultModulo;
-    }
-    printf("\n--------------------------\n");
-    printf("Ihr Ergebnis ist: %.4f",result);     //  %.4f .. format output float number to 4 decimal places after the comma
-    printf("  (auf 4 Nachkommastellen genau)");
-  }
-  else
+  printf("\n  TEMPERATURTABELLE: ");
+  printf("\n Celsius - Fahrenheit ");
+  printf("\n======================\n");  
+  for ( i = -5; i<=5; i++ )
   {
-    printf("! Ungueltiger Operator gewaehlt: %s !",operator);
+    printf( "   %3d   -   %3d\n", (i*10),(((i*10)*9)/5)+32);
   }
+
   //  END PROGRAM  //
-  printf("\nEingabetaste zum Beenden druecken.");
+  printf("----------------------");
+  printf("\n   Eingabetaste zum \n   Beenden druecken");
   while (getchar() != '\n');    //  wait for [Enter]-Key
   getchar();                    //
   return 0;
